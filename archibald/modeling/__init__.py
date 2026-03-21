1385a96a90f5d24d6ac6116fc35b66e1925497fa
Jules Richeux <jules.richeux22@gmail.com>
2026-03-21 21:41:12 +0100

    Add modeling module

---
+++ archibald/modeling/__init__.py
@@ -0,0 +1,6 @@
+from archibald.modeling.fitting import FittedModel
+from archibald.modeling.interpolation import InterpolatedModel
+from archibald.modeling.interpolation_unstructured import (
+    UnstructuredInterpolatedModel,
+)
+from archibald.modeling.black_box import black_box
