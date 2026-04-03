b919a27a98125ab19f5cf7a898129260163d0955
Jules Richeux <jules.richeux22@gmail.com>
2026-04-03 16:29:19 +0200

    Add geom_utils

--- archibald/geometry/__init__.py
+++ archibald/geometry/__init__.py
@@ -1,7 +1,8 @@
-from archibald.geometry.airfoil.airfoil import Airfoil
-from archibald.geometry.airfoil.kulfan_airfoil import KulfanAirfoil
-from archibald.geometry.propeller import Propeller, BSeriesPropeller
-from archibald.geometry.wing import Wing, WingXSec, ControlSurface, LiftingDevice, Sail, Fin
-from archibald.geometry.lifting_set import LiftingSet, Rig, Appendage
-from archibald.geometry.mesh_utilities import stack_meshes, convert_mesh_to_polydata_format
+from archibald.geometry.airfoil import *
+# from archibald.geometry.airfoil.airfoil import Airfoil
+# from archibald.geometry.airfoil.kulfan_airfoil import KulfanAirfoil
+# from archibald.geometry.propeller import Propeller, BSeriesPropeller
+# from archibald.geometry.wing import Wing, WingXSec, ControlSurface, LiftingDevice, Sail, Fin
+# from archibald.geometry.lifting_set import LiftingSet, Rig, Appendage
+# from archibald.geometry.mesh_utilities import stack_meshes, convert_mesh_to_polydata_format
 
